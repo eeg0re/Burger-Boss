de25c27a2a35da36d529603e51f75629bb689aae
Igor Bessa <ibessa@ucsc.edu>
2023-11-27 13:56:29 -0800

    added more file and scene structuring

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -4,11 +4,27 @@ class Load extends Phaser.Scene{
     }
 
     preload(){
+        // loading bar code provided by Prof. Nathan Altice
+        // code from Asset Bonanza repo on Github
+        // set up loading bar
+        // see: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/loader/
+        let loading = this.add.graphics();
+        // see: https://photonstorm.github.io/phaser3-docs/Phaser.Loader.Events.html
+        this.load.on('progress', (value)=> {
+            loading.clear();                            // reset fill/line style
+            loading.fillStyle(0x911C1C, 1);             // (color, alpha)
+            loading.fillRect(30, 300, game.config.width*value, 15);  // (x, y, width, height)
+        });
+        this.load.on('complete', ()=> {
+            loading.destroy();
+        });
+
+
 
     }
 
     create(){
-
+        this.scene.start('menuScene');
     }
 
     update(){
