262e71800083c77cd386496d176c25a7c41d3e33
Igor Bessa <ibessa@ucsc.edu>
2023-12-14 21:42:48 -0800

    added tweens

--- src/scenes/Control.js
+++ src/scenes/Control.js
@@ -12,12 +12,25 @@ class Control extends Phaser.Scene{
         this.add.image(width/2 + 25, height/4 + 192, 'left');
         this.add.image(width/2+100, height/4 + 192, 'right');
         this.add.bitmapText(width/2, height-50, 'bold-pixel', 'Press Space to continue').setOrigin(0.5).setScale(0.5);
+        
+        // create a chef for tweens 
+        this.chef = new Chef(this, -64, height-90, 'chef', 0, 'right').setScale(4);
+        this.chef.anims.play('chef-walk');
     }
     update(){
         if(this.cursors.space.isDown){
-            player_score = 0;
-            this.scene.start('level1Scene');
-            this.sound.play('sfx-UI');
+            this.chefTween = this.tweens.add({
+                targets: this.chef,
+                ease: 'Sine.easeInOut',
+                duration: 2000,
+                x: width+32,
+                stepX: 10,
+                onComplete: ()=>{
+                    player_score = 0;
+                    this.scene.start('level1Scene');
+                    this.sound.play('sfx-UI');
+                }
+            });
         }
     }
 }
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,16 +8,25 @@ class Menu extends Phaser.Scene{
         this.cameras.main.setBackgroundColor(0x000000);
         this.add.bitmapText(width/2, height/4 + 75, 'bold-pixel', 'Press SPACE to play').setOrigin(0.5);
         this.add.bitmapText(width/2 , height/4, 'bold-pixel', 'BURGER BOSS').setOrigin(0.5);
-        this.add.image(width/2, height/2 + 100, 'tsChef').setScale(10);
+        this.chef = this.add.image(width/2, height/2 + 100, 'tsChef').setScale(10);
 
         this.add.bitmapText(width/2, height-75, 'bold-pixel', 'Press shift to view credits').setOrigin(0.5).setScale(0.5);
     }
 
     update(){
         if(this.cursors.space.isDown){
-            player_score = 0;
-            this.scene.start('controlScene');
-            this.sound.play('sfx-UI');
+            let thisTween = this.tweens.add({
+                targets: this.chef,
+                scale: { from: 10, to: 0.5},
+                ease: 'Sine.easeInOut',
+                duration: 1000,
+                onComplete:()=>{
+                    player_score = 0;
+                    this.scene.start('controlScene');
+                    this.sound.play('sfx-UI');
+                }
+
+            });
         }
         if(this.cursors.shift.isDown){
             this.scene.start('creditScene');
